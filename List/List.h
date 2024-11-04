96966f6fa0579f0792aea255d70c74fec9659e9c
burhangln <burhangulen999@gmail.com>
2024-11-04 10:56:06 +0100

    Proje dosyası ekle.

---
+++ List/List.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <exception>
+#include <stdexcept>
+
+template <typename T>
+class List
+{
+private:
+	struct Node
+	{
+		Node(T value) noexcept
+		{
+			this->value = value;
+			next = nullptr;
+			prev = nullptr;
+		}
+		T value;
+		Node* next;
+		Node* prev;
+	};
+
+	Node* head;
+	Node* tail;
+
+public:
+	List() noexcept
+	{
+		head = nullptr;
+		tail = nullptr;
+	}
+
+	~List()
+	{
+		while (head != nullptr)
+		{
+			Node* tmp = head;
+			head = head->next;
+			delete tmp;
+		}
+	}
+
+	size_t size() const noexcept
+	{
+		Node* current = head;
+		size_t count = 0;
+
+		while (current != nullptr)
+		{
+			++count;
+			current = current->next;
+		}
+
+		return count;
+	}
+
+	void pushFront(T value)
+	{
+		Node* newNode = new Node(value);
+		newNode->next = head;
+		if (head != nullptr)
+			head->prev = newNode;
+		else
+			tail = newNode;
+		head = newNode;
+	}
+};
